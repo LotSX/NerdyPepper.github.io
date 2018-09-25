28e85a6309d4a2d4f2108f461ae5da7b15b809d8
Ray Chen <rchen25@ucsc.edu>
2018-09-25 16:16:14 -0700

    can put url in search_bar now, still needs to check for everything but https, will change when relevant

--- main.js
+++ main.js
@@ -75,9 +75,18 @@ function date() {
                                   date + oi + " " + today.getFullYear();
 }
 
+function is_url(str) {
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  return regexp.test(str)
+}
+
 function keyup(e) {
-  inputTextValue = e.target.value;
+  let inputTextValue = e.target.value;
+  let has_https = (/(http(s)?:\/\/.)/.test(inputTextValue));
+
   if (e.keyCode == 13) {
-    window.location = "https://duckduckgo.com/?q=" + inputTextValue;
+    if (is_url(inputTextValue))
+      window.location = has_https ? inputTextValue : "https://" + inputTextValue;
+    else window.location = "https://duckduckgo.com/?q=" + inputTextValue;
   }
 }
